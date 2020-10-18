fe816f14ff3eb15c89a34b7f0e299caa39115d66
csslick <digitone@naver.com>
2020-10-18 23:04:06 +0900

    animation fixed

--- js/main.js
+++ js/main.js
@@ -10,12 +10,17 @@ var visual = [
     bg: "./images/h1-slider-img-1.png",
     bg_txt: "./images/h1-slider-img-2.png",
     actor: "./images/h1-slider2-img-1.png"
+  },
+  { 
+    title: "work your<br> body", 
+    bg: "./images/h1-slider-img-1.png",
+    bg_txt: "./images/h1-slider-img-2.png",
+    actor: "./images/h1-slider3-img-1.png"
   }
 ];
 var visual_num = 0;
 
 function play_visual() {
-  $("#visual h1").removeClass("play");
   $("#visual h1").html(visual[visual_num].title);
   $("#visual .actor").attr('src', visual[visual_num].actor);
   // Visual animation play
@@ -25,18 +30,21 @@ function play_visual() {
   $("#visual .actor").addClass("play");
 }
 
+function init_visual() {
+  $("#visual h1").removeClass("play");
+  $("#visual .visual-bg").removeClass("play");
+  $("#visual .visual-text").removeClass("play");
+  $("#visual .actor").removeClass("play");
+}
+
 window.onload = function () {
   play_visual();
 
-  // setInterval(function(){
-  //   $("#visual h1").removeClass("play");
-  
-  //   if(visual_num >= visual.length-1) { visual_num = 0;} else { visual_num++; }
-  //   play_visual();
-  // }, 3000);
-  $('body').click(function(){
+  setInterval(function(){
+    init_visual();
+    console.log($("#visual h1").css('animation-play-state'));
     if(visual_num >= visual.length-1) { visual_num = 0;} else { visual_num++; }
-    play_visual(); 
-  })
+    play_visual();
+  }, 5000);
 
 };  // end onload
